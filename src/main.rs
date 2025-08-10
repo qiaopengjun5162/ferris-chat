f646e10a60b3c9152b425eee9b2d519697545eca
qiaopengjun5162 <qiaopengjun0@gmail.com>
2025-08-10 23:44:52 +0800

    feature: build api skeleton

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use ferris_chat::{AppConfig, get_router};
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{Layer as _, fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let config = AppConfig::load()?;
+    let addr = format!("0.0.0.0:{}", config.server.port);
+
+    let app = get_router(config);
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on {}", addr);
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
